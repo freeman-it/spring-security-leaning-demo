0a6079a741e0be74fb3eee8a9690bf271ebd027b
freeman <17789620826@189.cn>
2024-10-12 09:48:42 +0800

    hello

--- src/main/java/com/ilxqx/springsecuritylearning/controller/HomeController.java
+++ src/main/java/com/ilxqx/springsecuritylearning/controller/HomeController.java
@@ -24,6 +24,7 @@ public class HomeController {
 
     @GetMapping(value = "/index", produces = MediaType.APPLICATION_JSON_VALUE)
     public JsonResult<String> index() {
+
         System.out.println("git reset");
         return JsonResult.success("成功", this.homeService.sayHello());
     }
